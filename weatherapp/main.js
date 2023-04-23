7098a7d6a2b5345fc6c43a2735bb38436e139178
Vipavee_K <vkositthai@outlook.com>
2023-04-23 13:29:16 +0200

    Update main.js

--- weatherapp/main.js
+++ weatherapp/main.js
@@ -1,7 +1,3 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
-
-
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const UNSPLASH_API_KEY = process.env.UNSPLASH_API_KEY;
 
